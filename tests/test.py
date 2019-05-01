61b2fdcc128456a6260ae50b57e926928308bfe0
Takayuki Kobayashi <iris.takayuki@gmail.com>
2019-05-01 17:48:42 +0900

    update tests/test.py

--- tests/test.py
+++ tests/test.py
@@ -1,12 +1,6 @@
-import yep
-
 import cmake_example as m
-
-yep.start('out.prof')
 
 assert m.__version__ == '0.0.1'
 assert m.add(1, 2) == 3
 assert m.subtract(1, 2) == -1
-assert m.fib(40) == 102334155
-
-yep.stop()
+assert m.fib(45) == 1134903170
